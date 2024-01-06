580e3652bb2bd986467d9635cfcc1216f564dfec
Saiful <mdqwnsaiful@gmail.com>
2024-01-06 12:19:00 -0500

    Initial Commit

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -1,5 +1,11 @@
 package com.smart.tests;
 
 public class Test {
-
+	
+    @org.testng.annotations.Test
+    
+   
+	public void loginTest() {
+    	System.out.println("we are learning Git and Github through Eclipse");
+    }
 }
